f329fec236c2431fd247ba5c288d2909b79ecf63
Bhavasree_J_K <bhavasree025@gmail.com>
2025-07-28 20:18:25 +0530

    Time: 2 ms (37.12%) | Memory: 42.1 MB (39.23%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -21,31 +21,7 @@ class Solution {
     }
     if(val1&&val2)return true;
        
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<word.length();i++){
-        if(i==0){
-         if(Character.isUpperCase(word.charAt(i))){
-          continue;
-         }
-         else{
-            return false;
-         }
-        }
-        else if(i>0){
-            if(Character.isLowerCase(word.charAt(i))){
-                return false;
-            }
-
-           
-        }
-        }
-      
-   
-
-      
-      
-        
+       
     return false;
         
     }
