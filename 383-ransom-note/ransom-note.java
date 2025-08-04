371b3c96ce6d31cb772bcb8c70c655eb2660e1c1
Bhavasree_J_K <bhavasree025@gmail.com>
2025-08-04 21:17:14 +0530

    Time: 15 ms (22.22%) | Memory: 45.2 MB (37.76%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String r, String m) {
+       
+   HashMap<Character,Integer> map1=new HashMap<>();
+    HashMap<Character,Integer> map2=new HashMap<>();
+    for(char i: r.toCharArray()){
+        map1.put(i,map1.getOrDefault(i,0)+1);
+    }
+  
+     for(char i: m.toCharArray()){
+        map2.put(i,map2.getOrDefault(i,0)+1);
+    }
+   for(char c :map1.keySet()){
+    if(map2.getOrDefault(c,0)<map1.get(c)){
+        return false;
+    }
+   }
+   return true;
+    }
+}
+   
