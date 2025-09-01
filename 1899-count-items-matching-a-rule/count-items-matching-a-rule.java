418b957e76c8bd0dcb7f5eb1282b71d2dc7eed94
Bhavasree_J_K <bhavasree025@gmail.com>
2025-09-01 22:53:35 +0530

    Time: 22 ms (7.29%) | Memory: 47.1 MB (99.70%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+    ArrayList<String> list1=new ArrayList<>();
+        ArrayList<String> list2=new ArrayList<>();
+        ArrayList<String> list3=new ArrayList<>();
+
+    
+    for(int i=0;i<items.size();i++){
+    
+            list1.add(items.get(i).get(0));
+            }
+             for(int i=0;i<items.size();i++){
+    
+            list2.add(items.get(i).get(1));
+            }
+             for(int i=0;i<items.size();i++){
+    
+            list3.add(items.get(i).get(2));
+            }
+          
+    System.out.print(list1);
+    System.out.print(list2);
+    System.out.print(list3);
+
+    int count=0;
+    if(ruleKey.equals("type")){
+         for(int i=0;i<list1.size();i++ ){
+
+            if(list1.get(i).equals(ruleValue)){
+                count++;
+            }
+         }
+    }
+    else if(ruleKey.equals("color")){
+          for(int i=0;i<list2.size();i++ ){
+
+            if(list2.get(i).equals(ruleValue)){
+                count++;
+            }
+         }
+
+    }
+    else{
+      for(int i=0;i<list3.size();i++ ){
+
+            if(list3.get(i).equals(ruleValue)){
+                count++;
+            }
+         }
+    }
+    
+    
+    return count;
+    }
+}
