3abee1bb9883e31a9b4bc3555964cc38cbe14afa
Bhavasree_J_K <bhavasree025@gmail.com>
2025-09-05 21:12:51 +0530

    Time: 7 ms (2.79%) | Memory: 41.6 MB (51.25%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int rearrangeCharacters(String s, String t) {
+    StringBuilder sb=new StringBuilder(s);  
+    HashMap<Character,Integer> map=new HashMap<>();
+    for(int i=0;i<s.length();i++){
+        boolean val=false;
+        for(int j=0;j<t.length();j++){
+
+        if(s.charAt(i)==t.charAt(j)){
+        
+           val=true;
+           break;
+        }
+        }
+        if(val){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+      
+        }
+        
+
+    }
+
+     HashMap<Character,Integer> map1=new HashMap<>();
+    for(int i=0;i<t.length();i++){
+        map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)+1);
+   
+      
+
+    }
+System.out.print(map);
+System.out.print(map1);
+
+ if(map1.size()!=map.size()) return 0;
+ 
+ int min=Integer.MAX_VALUE;
+ for(char i :map.keySet()){
+    min=Math.min(min,map.get(i)/map1.get(i));
+
+ }
+      
+         return  min;
+    }
+}
