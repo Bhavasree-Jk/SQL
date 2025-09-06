fb713af2861d0c6822c505605ac2b9bcc67abd42
Bhavasree_J_K <bhavasree025@gmail.com>
2025-09-06 11:30:29 +0530

    Time: 8 ms (49.27%) | Memory: 42.1 MB (69.04%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -3,14 +3,14 @@ class Solution {
         if(t.length()<7){
             return 0;
         }
-         TreeMap<Character,Integer> map1=new TreeMap<>();
+         HashMap<Character,Integer> map1=new HashMap<>();
          String s="balloon";
     for(int i=0;i<s.length();i++){
         map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
     }
    
    
-    TreeMap<Character,Integer> map2=new TreeMap<>();
+    HashMap<Character,Integer> map2=new HashMap<>();
     for(int i=0;i<t.length();i++){
         boolean val=false;
         for(int j=0;j<s.length();j++){
