578bf0b495b84f80857c712b5317a53caf107ff9
Bhavasree_J_K <bhavasree025@gmail.com>
2025-09-15 22:55:13 +0530

    Time: 7 ms (9.05%) | Memory: 44.7 MB (5.34%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canBeTypedWords(String t, String b) {
+        String[] s=t.split(" ");
+        int count=0;
+        for(int i=0;i<s.length;i++){
+            boolean val=true;
+           for(int j=0;j<s[i].length();j++){
+            if(b.contains(s[i].charAt(j)+"")){
+                val=false;
+            }
+           }
+           if(val){
+            count++;
+           }
+        }
+        return count;
+    }
+}
